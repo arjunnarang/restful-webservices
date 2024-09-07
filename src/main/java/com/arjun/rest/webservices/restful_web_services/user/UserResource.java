3174fc81ce96ea3cf43af926fe007cd38d492982
arjunnarang <arjun.12narang@gmail.com>
2024-09-07 23:39:55 +0530

    implementing post method and correct status codes

--- src/main/java/com/arjun/rest/webservices/restful_web_services/user/UserResource.java
+++ src/main/java/com/arjun/rest/webservices/restful_web_services/user/UserResource.java
@@ -2,13 +2,16 @@
 
 package com.arjun.rest.webservices.restful_web_services.user;
 
+import java.net.URI;
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class UserResource {
@@ -27,13 +30,27 @@ public class UserResource {
 	
 	@GetMapping(path = "/users/{id}")
 	public User retrieveById(@PathVariable Integer id){
-		return service.findOne(id);
+		User user = service.findOne(id);
+		
+		if(user == null) {
+			throw new UserNotFoundException("id:" + id);
+		}
+		
+		return user;
 	}
 	
 	
 	//Creating new user, 
 	@PostMapping("/users")
-	public void createuser(@RequestBody User user) {
-		service.save(user);
+	public ResponseEntity<User> createuser(@RequestBody User user) {
+		User savedUser = service.save(user);
+		
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest()  //this will return the current request that is /users
+												  .path("/{id}")         //this will append the id of the newly created user on to current requests
+												  .buildAndExpand(savedUser.getId())  //this will get the value of the created or savedUser and place in the /users/{id}
+												  .toUri();  //this will compile the location url as localhost:8080/users/4
+		
+		//return http response status 201 which stands for created and location created above to fetch the currently created user as a response
+		return ResponseEntity.created(location).build();
 	}
 }
