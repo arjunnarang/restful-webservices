f012e7653178628c1f1aefd3bc1f50b91d20766b
arjunnarang <arjun.12narang@gmail.com>
2024-09-14 16:34:41 +0530

    Internationalization of project

--- src/main/java/com/arjun/rest/webservices/restful_web_services/helloworld/HelloWorldController.java
+++ src/main/java/com/arjun/rest/webservices/restful_web_services/helloworld/HelloWorldController.java
@@ -1,11 +1,23 @@
 package com.arjun.rest.webservices.restful_web_services.helloworld;
 
+import java.util.Locale;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
+	
+	//Spring defined class to handle internationalization of the project
+	private MessageSource messageSource;
+	
+	//Constructor injection
+	public HelloWorldController(MessageSource messageSource) {
+		this.messageSource = messageSource;
+	}
 	
 	@GetMapping(path = "/hello-world")
 	public String helloWorld() {
@@ -24,4 +36,15 @@ public class HelloWorldController {
 	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
 		return new HelloWorldBean("Hello World");
 	}
+	
+	@GetMapping(path = "/hello-world-internationalized")
+	public String helloWorldInternationalized() {
+		
+		//If a user sends a Accept header then LocaleContextHolder will return that locale otherwise local locale is sent
+		Locale locale = LocaleContextHolder.getLocale();
+		
+		return messageSource.getMessage("good.morning.message", null, "Default Message", locale);
+		//return "Hello World V2";
+	}
+
 }
